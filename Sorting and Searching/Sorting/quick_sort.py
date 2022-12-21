147b313b31b02c459acb9ae56a569e1695a381eb
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2022-12-21 18:38:09 +0100

    Add quick_sort.py

---
+++ Sorting and Searching/Sorting/quick_sort.py
@@ -0,0 +1,88 @@
+import random
+
+
+def random_pivot(arr):
+    return random.randint(0, len(arr) - 1)
+
+
+def median_of_three_pivot(arr, low: int, high: int):
+    low_val = arr[low]
+    mid_val = arr[(low + high) // 2]
+    high_val = arr[high]
+
+    if low_val <= mid_val:
+        if mid_val <= high_val:
+            return (low + high) // 2
+        elif high_val <= low_val:
+            return low
+        else:
+            return high
+    else:
+        if high_val >= low_val:
+            return low
+        elif mid_val >= high_val:
+            return (low + high) // 2
+        else:
+            return high
+
+
+def first_pivot(arr):
+    return 0
+
+
+def last_pivot(arr):
+    return len(arr) - 1
+
+
+def partition(arr, low, high):
+
+    def swap(i, j):
+        (arr[i], arr[j]) = (arr[j], arr[i])
+
+    pivot_index = median_of_three_pivot(arr, low, high)
+
+    # Our partition algorithm requires that the pivot be the last element
+    swap(pivot_index, high)
+
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            swap(i, j)
+
+    swap(i + 1, high)
+    return i + 1
+
+
+def quick_sort(arr: list, low: int, high: int) -> list:
+    if low < high:
+        pivot = partition(arr, low, high)
+        quick_sort(arr, low, pivot - 1)
+        quick_sort(arr, pivot + 1, high)
+    return arr
+
+
+def test_merge_sort():
+    for i in range(1, 101):
+        arr = [random.randint(0, 100) for _ in range(10)]
+
+        low = 0
+        high = len(arr) - 1
+
+        pivot_type = random.randint(0, 3)
+        match pivot_type:
+            case 0:
+                pivot_type = first_pivot(arr)
+            case 1:
+                pivot_type = last_pivot(arr)
+            case 2:
+                pivot_type = median_of_three_pivot(arr, low, high)
+
+        assert quick_sort(arr, low, high) == sorted(arr)
+        print(f"Test {i} passed")
+
+
+if __name__ == "__main__":
+    test_merge_sort()
