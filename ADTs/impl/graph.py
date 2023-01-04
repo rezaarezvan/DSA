0db3d4e7be59f2ee290f83302607c52b6bfabf48
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2023-01-04 18:35:55 +0100

    Update graph.py

--- ADTs/impl/graph.py
+++ ADTs/impl/graph.py
@@ -1,12 +1,12 @@
-'''
-A program to represent graphs in python
-'''
-
 import random
 import queue_linked_list
 
 
 class Edge:
+    '''
+    Auxilary Edge class for the Graph class.
+    '''
+
     def __init__(self, src, dst, weight=1):
         self.src = src
         self.dst = dst
@@ -17,6 +17,39 @@ class Edge:
 
 
 class Graph:
+    '''
+    A class that represents a directed graph using adjacency list representation
+
+    Attributes:
+        graph (dict): A dictionary representing the graph as adjacency list
+        n_edges (int): Number of edges in the graph
+        all_vertices (set): A set of all vertices in the graph
+
+    Description:
+        outgoing_edges(v) returns a list of all edges that have v as their source
+        add(edge) adds an edge to the graph
+        remove(edge) removes an edge from the graph
+
+        recursive_dfs(v, visited) performs a recursive depth first search on the graph
+
+        Note: DFS (Depth First Search) is an algorithm for traversing a Graph
+
+        It goes as deep as possible (visits all the children of a node)
+        before before backtracking to the parent node and visiting the next child.
+
+        iterative_BFS(v) performs an iterative breadth first search on the graph
+
+        Note: BFS (Breadth First Search) is an algorithm for traversing a Graph
+
+        It visits all the nodes at a given level before going to the next level.
+        (If we think of the graph as a tree)
+
+        In a arbitrary graph, the BFS uses a queue to keep track of the nodes to visit.
+        The queue is initialized with the starting node.
+        The algorithm then visits the first node in the queue, enqueues all of its children to the queue
+        and then dequeues the first node from the queue. This process is repeated until the queue is empty.
+    '''
+
     def __init__(self):
         self.all_edges = {}
         self.n_edges = 0
